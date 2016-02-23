e41676ffc02f7309ef972e864b3558c2da386290
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-23 19:01:21 +0900

    add name and nearest_station column to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,11 @@ class ApplicationController < ActionController::Base
   def sign_in_required
     redirect_to new_user_session_url unless user_signed_in?
   end
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :nearest_station
+  end
 end
