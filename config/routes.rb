768d3af9742fed9e3b62511e558aca8d23c0bdcd
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-25 18:06:00 +0900

    add defaults profile img

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   post 'post', to: 'food#post'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'pages#index'
 
